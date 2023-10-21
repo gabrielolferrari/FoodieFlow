351506a14ab9ec15348cfa3bb66d02e80c0f875a
Gabriel Ferrari <gabrielolferrari@gmail.com>
2023-10-21 10:00:40 -0300

    Add Pedidos

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ from infrastructure.database import init_db;
 from application.entrypoint import cliente_controller
 from application.entrypoint import categoria_controller
 from application.entrypoint import produto_controller
-from core.model.orm import cliente, categoria, imagem, ingrediente, produto
+from application.entrypoint import pedido_controller
+from core.model.orm import status, cliente, categoria, imagem, ingrediente, produto, pedido
 
 HOST = config('HOST_API', default='localhost')
 PORT = config('PORT_API', default='8000')
@@ -31,6 +32,7 @@ app.router.redirect_slashes = True
 app.include_router(cliente_controller.router, prefix='/clientes')
 app.include_router(categoria_controller.router, prefix='/categorias')
 app.include_router(produto_controller.router, prefix='/produtos')
+app.include_router(pedido_controller.router, prefix='/pedidos')
 
 @app.on_event("startup")
 async def startup_event():
