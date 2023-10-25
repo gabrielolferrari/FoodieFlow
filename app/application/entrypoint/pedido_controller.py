c853a0693287716c4f5c9a702894514645dfc5fe
Gabriel Ferrari <gabrielolferrari@gmail.com>
2023-10-25 10:39:46 -0300

    Ajuste de arquivos para entrega

--- app/application/entrypoint/pedido_controller.py
+++ app/application/entrypoint/pedido_controller.py
@@ -5,6 +5,7 @@ from core.usecases.pedido_service_impl import PedidoServiceImpl
 from core.ports.pedido_repository import PedidoRepository
 from infrastructure.dataprovider.pedido_database_adapter import PedidoDatabaseAdapter
 from core.model.pedido import Pedido as PedidoModel
+from application.commons.enums.status import StatusEnum
 from infrastructure.database import get_db
 
 router = APIRouter()
@@ -49,8 +50,22 @@ def read_pedidos_by_status(status: int, skip: int = 0, limit: int = 100, db: Ses
     except Exception as ex:
         log.error(f"Erro ao buscar pedidos. {str(ex)}")
         raise HTTPException(status_code=400, detail="Erro ao buscar pedidos")
+    
+@router.get("/preparacao", response_model=list[PedidoModel], description="Listar todos os pedidos Em Preparação")
+def read_pedidos_by_status(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    try:
+        log.info(f"Buscando pedidos com status 2")
+        pedidos = pedido_service.get_pedidos_by_status(db, StatusEnum.EM_PREPARACAO.value, skip, limit)
+        if not pedidos:
+            raise HTTPException(status_code=404, detail=f"Pedidos não encontrados com o status 2")
+        return pedidos
+    except HTTPException:
+        raise
+    except Exception as ex:
+        log.error(f"Erro ao buscar pedidos. {str(ex)}")
+        raise HTTPException(status_code=400, detail="Erro ao buscar pedidos")
 
-@router.put("/pagamento/{pedido_id}", description="Atualiza o status do pedido para 'Em preparação'")
+@router.put("/checkout/{pedido_id}", description="Atualiza o status do pedido para 'Em preparação'")
 def update_pedido_to_preparation(pedido_id: int, db: Session = Depends(get_db)):
     try:
         log.info(f"Atualizando status do pedido {pedido_id} para 'Em preparação'")
